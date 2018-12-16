0df569205ae992edc29623bb78763ee3b6efaaec
Issao Jr <issaojr@gmail.com>
2018-12-15 23:47:39 -0200

    Adicionado controller de páginas estáticas

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get sobre" do
+    get static_pages_sobre_url
+    assert_response :success
+  end
+
+end
